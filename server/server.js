2c23bbd7858110a1e0527321ea21b8c6a83182c2
unknown <arfaouisaber140@gmail.com>
2023-09-25 10:22:22 +0200

    test

---
+++ server/server.js
@@ -0,0 +1,32 @@
+
+const express = require('express');
+const cors = require('cors');
+const Bot = require('node-telegram-bot-api');
+const app = express();
+
+const token = '6647327011:AAErnbPs_krurmVxMbgai1SnWMYerC5-0lo'; // Replace with your Telegram bot token
+app.use(cors());
+app.use(express.json()); // Middleware to parse JSON request bodies
+
+
+const bot = new Bot(token, { polling: true });
+
+
+app.post('/', async (req, res) => {
+  const { msg } = req.body;
+
+  if (!msg) {
+    return res.status(400).json({ error: 'Message (msg) is required in the request body' });
+  }
+
+  try {
+    // Send the message to Telegram
+    bot.sendMessage('5881759396', msg); // Replace 'YOUR_CHAT_ID' with the chat ID of the recipient
+  } catch (error) {
+    console.error('Error saving DATA:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
