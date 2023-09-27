b45feb2d6ab0c724a01d189d6fcf56d16b2fa6b8
unknown <arfaouisaber140@gmail.com>
2023-09-27 22:14:14 +0200

    change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const Bot = require('node-telegram-bot-api');
 const app = express();
-
+// comment
 const token = '6647327011:AAErnbPs_krurmVxMbgai1SnWMYerC5-0lo'; // Replace with your Telegram bot token
 app.use(cors());
 app.use(express.json()); // Middleware to parse JSON request bodies
