443ac0988da0d283ce23d9716e556cbbfb3a08e2
Sabeur Arfaoui <117772841+arfaouisabeur@users.noreply.github.com>
2023-09-29 16:25:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ app.post('/', async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
